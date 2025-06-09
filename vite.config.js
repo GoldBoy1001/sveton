6a82fdf5bb1092556dc824ca358dad1742653852
GoldBoy1001 <chaplinsergey29@gmail.com>
2025-06-09 17:08:10 +0300

    added catalog

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/sveton/", // ← укажи точно имя репозитория!
+  base: "/ымуещт", // или /имя-репозитория/ для GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "catalog.html"),
+      },
+    },
+  },
 });
